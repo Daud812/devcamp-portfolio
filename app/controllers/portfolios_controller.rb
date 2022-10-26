ac834f3d0e2048ab314b069d50c55c98895f39a9
Daud812 <daud.saleem@codingcops.com>
2022-10-26 20:26:34 +0500

    integerated new and create action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,27 @@ class PortfoliosController < ApplicationController
     def index
         @portfolio=Portfolio.all
     end
+    def new
+    @portfolio = Portfolio.new
+    end
+
+    def create
+        @portfolio = Portfolio.new(portfolio_params)
+    
+        respond_to do |format|
+          if @portfolio.save
+            format.html { redirect_to portfolios_path, notice: 'Portfolio  created.' }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+          end
+        end
+    end
+
+     
+
+
+      def portfolio_params
+        params.require(:portfolio).permit(:titel,:subtitel, :body)
+      end
+
 end
