225fc36cb16cc77762a03d57f4d1c2fd3adc255c
Daud812 <daud.saleem@codingcops.com>
2022-10-28 15:01:49 +0500

    implement validation

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
-    enum status: {draft: 0, published: 1}
+  enum status: {draft: 0, published: 1}
   extend FriendlyId
   friendly_id :titel, use: :slugged
+
+  validates_presence_of :titel , :body
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
     extend FriendlyId
     friendly_id :titel ,use: :slugged
+
+    validates_presence_of :titel, :body
 end
